745ba0c060496622c57901c144d51ab4cd53df8e
Florian Rampp <Florian.Rampp@gmail.com>
2013-11-12 11:22:10 +0100

    added option to dump html

--- migrator.py
+++ migrator.py
@@ -21,11 +21,12 @@ def download_trip(args):
     _verify_http_url(args.trip_url)
     trip = parse_trip(args.trip_url)
 
-    #for entry in trip.entries:
-    #    print('{}:\n{}\n\n\n\n'.format(entry.title, entry.text))
-
     assert trip.number_entries == len(trip.entries)
     assert trip.number_photos == sum(map(lambda entry: len(entry.photos), trip.entries))
+
+    if args.dump_html:
+        for entry in trip.entries:
+            args.dump_html.write('{}:\n{}\n\n\n\n'.format(entry.title, entry.text))
 
     print(yaml.dump(trip))
 
@@ -34,19 +35,27 @@ def download_entry(args):
     _verify_http_url(args.entry_url)
     entry = parse_entry(args.entry_url)
 
+    if args.dump_html:
+        args.dump_html.write('{}:\n{}\n\n\n\n'.format(entry.title, entry.text))
+
     print(yaml.dump(entry))
 
 
 parser = argparse.ArgumentParser()
+
 subparsers = parser.add_subparsers(dest='subparser_name')
 
-parser_prepare = subparsers.add_parser('download-trip', help='download a whole trip from www.mytb.org')
-parser_prepare.set_defaults(func=download_trip)
-parser_prepare.add_argument(dest='trip_url')
+parser_download_trip = subparsers.add_parser('download-trip', help='download a whole trip from www.mytb.org')
+parser_download_trip.set_defaults(func=download_trip)
+parser_download_trip.add_argument(dest='trip_url')
+parser_download_trip.add_argument('--dump-html', type=argparse.FileType('w'))
 
-parser_prepare = subparsers.add_parser('download-entry', help='download a single entry from www.mytb.org')
-parser_prepare.set_defaults(func=download_entry)
-parser_prepare.add_argument(dest='entry_url')
+
+parser_download_entry = subparsers.add_parser('download-entry', help='download a single entry from www.mytb.org')
+parser_download_entry.set_defaults(func=download_entry)
+parser_download_entry.add_argument(dest='entry_url')
+parser_download_entry.add_argument('--dump-html', type=argparse.FileType('w'))
+
 
 args = parser.parse_args()
 args.func(args)
