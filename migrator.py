ee577cbda3197c7bafc2aa37ae8cea0f1a324b06
Florian Rampp <Florian.Rampp@gmail.com>
2013-11-14 11:29:56 +0100

    removed option for HTML dumping

--- migrator.py
+++ migrator.py
@@ -25,19 +25,12 @@ def download_trip(args):
     assert trip.number_entries == len(trip.entries)
     assert trip.number_photos == sum(map(lambda entry: len(entry.photos), trip.entries))
 
-    if args.dump_html:
-        for entry in trip.entries:
-            args.dump_html.write('{}:\n{}\n\n\n\n'.format(entry.title, entry.text))
-
     print(yaml.dump(trip))
 
 
 def download_entry(args):
     _verify_http_url(args.entry_url)
     entry = parse_entry(args.entry_url)
-
-    if args.dump_html:
-        args.dump_html.write('{}:\n{}\n\n\n\n'.format(entry.title, entry.text))
 
     print(yaml.dump(entry))
 
@@ -54,13 +47,11 @@ subparsers = parser.add_subparsers(dest='subparser_name', title='subcommands')
 parser_download_trip = subparsers.add_parser('download-trip', help='download a whole trip from www.mytb.org')
 parser_download_trip.set_defaults(func=download_trip)
 parser_download_trip.add_argument(dest='trip_url')
-parser_download_trip.add_argument('--dump-html', type=argparse.FileType('w'))
 
 
 parser_download_entry = subparsers.add_parser('download-entry', help='download a single entry from www.mytb.org')
 parser_download_entry.set_defaults(func=download_entry)
 parser_download_entry.add_argument(dest='entry_url')
-parser_download_entry.add_argument('--dump-html', type=argparse.FileType('w'))
 
 
 parser_wp_access_token = subparsers.add_parser('wp-access-token', help='retrieve the access token for a wordpress blog')
